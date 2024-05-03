d100bbbfa3f727e68716c9a0df94f48a2a4819ed
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-05-03 17:59:22 +0530

    Initial commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { ThirdwebProvider } from "@/app/thirdweb";
 import { Analytics } from '@vercel/analytics/react';
-import { SpeedInsights } from '@vercel/speed-insights/next';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -24,7 +23,6 @@ export default function RootLayout({
           {children}
         </ThirdwebProvider>
         <Analytics />
-        <SpeedInsights />
       </body>
     </html>
   );
