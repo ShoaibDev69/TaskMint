d6a004f69dc8b3d247e368cbc5692ec70106d344
ShoaibDev69 <shoaib.developer69@gmail.com>
2024-05-29 18:59:02 +0530

    refactor: Update method names and remove unused code in Accountability component

--- components/Accountability.tsx
+++ components/Accountability.tsx
@@ -1,75 +1,3 @@
-// 'use client';
-
-// import { chain } from "@/app/chain";
-// import { client } from "@/app/client";
-// import { ConnectButton, useActiveAccount, useReadContract } from "thirdweb/react";
-// import { contract } from "../utils/contracts";
-// import { Deposit } from "./Deposit";
-// import { toEther } from "thirdweb";
-// import { TasksList } from "./TasksList";
-// import { AddTask } from "./AddTask";
-
-// export const Accountability = () => {
-//     const account = useActiveAccount();
-
-//     const { data: depositAmount } = useReadContract({
-//         contract: contract,
-//         method: "getDepositAmount",
-//     });
-
-//     const { data: taskCount } = useReadContract({
-//         contract: contract,
-//         method: "getTaskCount"
-//     });
-
-//     const {
-//         data: lockedFundsAmount,
-//         isLoading: isLoadingLockedFundsAmount
-//     } = useReadContract({
-//         contract: contract,
-//         method: "getDepositAmount"
-//     });
-
-//     if (account) {
-//         return (
-//             <div style={{ textAlign: "center", minWidth: "500px" }}>
-//                 <ConnectButton
-//                     client={client}
-//                     chain={chain}
-//                 />
-//                 <div>
-//                     {depositAmount?.toString() === "0" && taskCount?.toString() === "0" ? (
-//                         <Deposit />
-//                     ) : depositAmount?.toString() !== "0" && taskCount?.toString() === "0" ? (
-//                         <TasksList />
-//                     ) : (
-//                         <>
-//                             {!isLoadingLockedFundsAmount && lockedFundsAmount !== undefined && (
-//                                 <div style={{ marginTop: "20px" }}>
-//                                     <h3>Locked Funds: {toEther(lockedFundsAmount)}</h3>
-//                                     <p style={{ fontSize: "12px" }}>Funds will be returned once all tasks are completed.</p>
-//                                     <AddTask />
-//                                     <TasksList />
-//                                 </div>
-//                             )}
-//                         </>
-//                     )}
-//                 </div>
-//             </div>
-//         );
-//     } else {
-//         return (
-//             <div style={{ textAlign: "center", minWidth: "500px" }}>
-//                 <ConnectButton
-//                     client={client}
-//                     chain={chain}
-//                 />
-//                 <p>Please connect your wallet.</p>
-//             </div>
-//         );
-//     }
-// };
-
 'use client';
 
 import { chain } from "@/app/chain";
@@ -91,22 +19,24 @@ export const Accountability = () => {
 
     const { data: taskCount } = useReadContract({
         contract: contract,
-        method: "getTaskCount",
+        method: "getTaskCount"
     });
 
     const {
         data: lockedFundsAmount,
-        isLoading: isLoadingLockedFundsAmount,
+        isLoading: isLoadingLockedFundsAmount
     } = useReadContract({
         contract: contract,
-        method: "getDepositAmount",
+        method: "getDepositAmount"
     });
 
     if (account) {
         return (
             <div style={{ textAlign: "center", minWidth: "500px" }}>
-                {/* This line is removed to remove the button in the red box */}
-                {/* <ConnectButton client={client} chain={chain} /> */}
+                <ConnectButton
+                    client={client}
+                    chain={chain}
+                />
                 <div>
                     {depositAmount?.toString() === "0" && taskCount?.toString() === "0" ? (
                         <Deposit />
@@ -117,9 +47,7 @@ export const Accountability = () => {
                             {!isLoadingLockedFundsAmount && lockedFundsAmount !== undefined && (
                                 <div style={{ marginTop: "20px" }}>
                                     <h3>Locked Funds: {toEther(lockedFundsAmount)}</h3>
-                                    <p style={{ fontSize: "12px" }}>
-                                        Funds will be returned once all tasks are completed.
-                                    </p>
+                                    <p style={{ fontSize: "12px" }}>Funds will be returned once all tasks are completed.</p>
                                     <AddTask />
                                     <TasksList />
                                 </div>
@@ -132,7 +60,10 @@ export const Accountability = () => {
     } else {
         return (
             <div style={{ textAlign: "center", minWidth: "500px" }}>
-                <ConnectButton client={client} chain={chain} />
+                <ConnectButton
+                    client={client}
+                    chain={chain}
+                />
                 <p>Please connect your wallet.</p>
             </div>
         );
